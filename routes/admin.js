18d915f32735e1c454413c2f0ff71b701bb4a756
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-03-05 13:44:02 +0700

    feat: adding rouote for post

--- routes/admin.js
+++ routes/admin.js
@@ -5,4 +5,22 @@ router.get("/", (req, res, next) => {
   res.render("./admin/admin");
 });
 
+router.get("/new", (req, res, next) => {
+  const date = new Date().toISOString().split("T")[0];
+  res.render("./admin/new", {
+    publishDate: date,
+  });
+});
+
+router.get("/edit/:productId", (req, res, next) => {
+  res.render("./admin/edit");
+});
+
+// Post
+
+router.post("/new", (req, res, next) => {
+  const body = req.body;
+  console.log(body);
+});
+
 module.exports = router;
